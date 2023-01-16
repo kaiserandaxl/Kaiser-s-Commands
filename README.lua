38b530ff9d0b141832a54b860e3fc0173f4c583f
kaiserandaxl <122803145+kaiserandaxl@users.noreply.github.com>
2023-01-16 08:20:23 -0600

    Update and rename README.md to README.lua

---
+++ README.lua
@@ -0,0 +1,65 @@
+--[[
+# Kaiser-s-Commands
+My admin commands
+Hello, this is version 1.0.0 of Kaiser's admin commands I'll teach you how to configure the commands to work for you, and do what you want.
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+
+By default Owner, Administrator, and Moderator are enabled as commands, each command has a configured rank attached to it, you can change it.
+
+You're also able to add customisable ranks if the ones aren't enough, just add a nubmer value to Ranks, and name the permissions accordingly.
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+
+Commands Owners can access by default: All
+
+Commands Administrator can access by default: Ban, Restart, and all Moderator commands.
+
+Commands Moderators can access by default: Strip, Slay, Slap, Setjob, Jail, Heal, Hp, Gotoland, God, Freeze, To, Bring, Return
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+
+How to configure Rank permissions:
+In the value put the first rank that can access it (The number, not the name)
+
+after you set it you will have to configure what ranks can access each command.
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+
+How to configure Command Permissions:
+Under the Permissions Folder you'll see the list of all the commands, in the value put the name of the rank you want to access it.
+
+After this is configured the commands will detect the change, and automatically update.
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+
+Jail Location:
+You setup the jail location by setting the object value of Jail
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+
+Adminland: (Not required)
+Do the same thing has the Jail object value
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+
+You MUST set the group ID under ranks to the group you want it configured to! If this is not done your commands will not work.
+
+All scripts are obfuscated, and do not cause harm. I will post a document with-in the pastebin to understand how to edit stuff better.
+
+You must give your staff the commands list from the pastebin at the bottom of this.
+
+Each command is lowercase.
+
+Love From,
+kaiserandaxl
+
+The Team:
+Scripters:
+kaiserandaxl
+
+UI Designers:
+kaiserandaxl
+
+Go here for the list of commands and what they do: https://pastebin.com/6p9bYFeV
+]]
--- README.md
+++
@@ -1,2 +0,0 @@
-# Kaiser-s-Commands
-My admin commands
